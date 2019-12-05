0b2dc9d9f50d9a914724abd7137bccf35f939f5b
严宁宁 <yanningning@jiuqi.com.cn>
2019-12-05 11:32:38 +0800

    fix div out of range of the body

--- src/resize/index.js
+++ src/resize/index.js
@@ -7,32 +7,31 @@ class Resize {
     _resizeEl = null
     _handlerList = []
     handlerType = null
-    constructor () {
-
+    constructor ({handlerList, resizeEl}) {
+      this._handlerList = handlerList
+      this._resizeEl = document.querySelector(resizeEl)
     }
     init () {
         // 获取rezise元素
         // resize 绑定事件
         let that = this
-        this._handlerList = [
-            'left-top',
-            'right-top',
-            'left-bottom',
-            'right-bottom'
-        ]
         // let handlerEl = document.querySelector('.right-top')
+      /**
+       * 另一种做法是再需要设置拖动的元素上绑定mousedown
+           *  dropEl.addEventListener('mousedown', (e) => {
+           *    e.preventDefault()
+           *    window.addEventListener('mousemove', resize)
+           *    window.addEventListener('mouseup', removeResize)
+           *  }, false)
+       */
         let handlerEl = document.body
         handlerEl.addEventListener('mousedown', (e) => this.mouseDown(e), false)
         handlerEl.addEventListener('mousemove', (e) => this.mouseMove(e), false)
         handlerEl.addEventListener('mouseup', (e) => this.mouseUp(e), false)
         // 获取需要设置宽度的元素
-        this._resizeEl = document.querySelector('.resize-container')
     }
     mouseDown (e) {
-        console.log(e.target)
-        console.log(e.target.dataset.position)
         let index = this._handlerList.indexOf(e.target.dataset.position)
-        console.log('index', index)
         this.handlerType = [
             {
                 type: 'left-top',
@@ -82,12 +81,12 @@ class Resize {
     }
     mouseUp () {
         this._resizeActive = false
-        console.log('mouseup', this._resizeActive)
     }
     mouseMove (e) {
         if ( this._resizeActive) {
             let changeY = e.pageY - this._startPos.y
             let changeX = e.pageX - this._startPos.x
+
             this._resizeEl.style.width = this._startSize.width 
             + this.handlerType.width * changeX 
             + 'px'
@@ -95,9 +94,11 @@ class Resize {
             this._resizeEl.style.height = this._startSize.height 
             + this.handlerType.height * changeY 
             + 'px'
+
             this._resizeEl.style.top = this._startSize.top 
             + this.handlerType.top * changeY
             + 'px'
+
             this._resizeEl.style.left = this._startSize.left 
             + this.handlerType.left * changeX
             + 'px'
@@ -105,5 +106,8 @@ class Resize {
     }
 }
 
-let resizer = new Resize()
+let resizer = new Resize({
+  handlerList: ['left-top', 'right-top', 'left-bottom', 'right-bottom'], // dataset-option 列表
+  resizeEl: '.resize-container'
+})
 resizer.init()
